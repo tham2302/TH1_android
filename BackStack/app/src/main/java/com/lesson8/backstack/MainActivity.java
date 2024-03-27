206a53bc141ee02c6036150e7de9c5355b3b486c
Trần Thắm <tranthitham23022002@gmail.com>
2024-03-27 10:56:23 +0700

    hehe

--- BackStack/app/src/main/java/com/lesson8/backstack/MainActivity.java
+++ BackStack/app/src/main/java/com/lesson8/backstack/MainActivity.java
@@ -1,11 +1,17 @@
 package com.lesson8.backstack;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.view.View;
+
+import com.lesson8.backstack.model.FragmentA;
+import com.lesson8.backstack.model.FragmentB;
+import com.lesson8.backstack.model.FragmentC;
 
 public class MainActivity extends AppCompatActivity {
     private FragmentManager manager;
@@ -19,6 +25,52 @@ public class MainActivity extends AppCompatActivity {
     }
     private void add(Fragment fg, String tag, String name){
         transaction=manager.beginTransaction();
-        transaction.add(R.id.frame,fg,tag);
+        transaction.add(R.id.frame, fg, tag);
+        transaction.addToBackStack(name);
+        transaction.commit();
+    }
+    private void addA(View view){
+        FragmentA fg= new FragmentA();
+        add(fg, "fragA", "fa");
+    }
+    private void addB(View view){
+        FragmentB fg= new FragmentB();
+        add(fg, "fragB", "fb");
+    }
+    private void addC(View view){
+        FragmentC fg= new FragmentC();
+        add(fg, "fragC", "fc");
+    }
+    private void remove(Fragment fg, String tag){
+        transaction=manager.beginTransaction();
+        fg=manager.findFragmentByTag(tag);
+        transaction.remove(fg);
+        transaction.commit();
+    }
+    private void removeA(View view){
+        FragmentA fg=new FragmentA();
+        remove(fg, "fragA");
+    }
+    private void removeB(View view){
+        FragmentB fg=new FragmentB();
+        remove(fg, "fragB");
+    }
+    private void removeC(View view){
+        FragmentC fg=new FragmentC();
+        remove(fg, "fragC");
+    }
+    public void back(View view){
+        manager.popBackStack();
+    }
+    public void popA(View view){
+        manager.popBackStack("fa", 0);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(manager.getBackStackEntryCount()>0){
+            manager.popBackStack();
+        }
+        super.onBackPressed();
     }
 }
--- BackStack/app/src/main/java/com/lesson8/backstack/model/FragmentC.java
+++ BackStack/app/src/main/java/com/lesson8/backstack/model/FragmentC.java
@@ -19,7 +19,7 @@ public class FragmentC extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_a, container, false);
+        View view=inflater.inflate(R.layout.fragment_c, container, false);
         return view;
     }
 
